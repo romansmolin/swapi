4fcfaeaf4febef887fcf3a2d4785c6b521ff2ac5
Romans Smolins <romans.smolins@fintecor.com>
2024-08-30 13:22:51 +0300

    Fix list card

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -4,39 +4,40 @@ import { Button } from '../ui'
 
 const CharacterCard: React.FC<Character> = ({ name, gender, height, homeworld, id }) => {
     return (
-        <div className="block rounded-lg shadow-lg text-center">
-            <div className="p-6 space-y-5">
+        <div className="rounded-lg shadow-lg text-center flex flex-col justify-between h-full">
+            <div className="p-6 space-y-5 flex-grow">
                 <h5 className="text-xl font-bold tracking-wide text-primary-800">
                     {name}
                 </h5>
+            </div>
 
-                <div className="text-sm ">
-                    <p>
-                        <span className="font-semibold text-primary-800">Homeworld: </span>
-                        {homeworld.name}
-                    </p>
-                    <p>
-                        <span className="font-semibold text-primary-800">Gender: </span>
-                        {gender.charAt(0).toUpperCase() + gender.slice(1)}
-                    </p>
-                    <p>
-                        <span className="font-semibold text-primary-800">Height: </span>
-                        {height} cm
-                    </p>
-                </div>
+            <div className="text-sm">
+                <p>
+                    <span className="font-semibold text-primary-800">Homeworld: </span>
+                    {homeworld.name}
+                </p>
+                <p>
+                    <span className="font-semibold text-primary-800">Gender: </span>
+                    {gender.charAt(0).toUpperCase() + gender.slice(1)}
+                </p>
+                <p>
+                    <span className="font-semibold text-primary-800">Height: </span>
+                    {height} cm
+                </p>
+            </div>
 
-                <Button 
+            <div className="p-6">
+                <Button
                     isLink
                     href={{
                         pathname: '/character-details',
                         query: { id: id },
                     }}
-                 >
-                        Read now
+                >
+                    Read now
                 </Button>
             </div>
         </div>
-
     )
 }
 
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header: React.FC = () => {
@@ -6,9 +7,9 @@ const Header: React.FC = () => {
             <div className="mx-auto shadow-md flex h-16 items-center gap-8 px-4 sm:px-6 lg:px-8">
                 <a className="block" href="#">
                     <span className="sr-only">Home</span>
-                    <h2 className="text-4xl font-bold bg-gradient-to-r from-primary-600 to-indigo-900 bg-clip-text text-transparent">
+                    <Link href="/" className="text-4xl font-bold bg-gradient-to-r from-primary-600 to-indigo-900 bg-clip-text text-transparent">
                         SWAPI
-                    </h2>
+                    </Link>
                 </a>
             </div>
         </header>
